612c76c7a2a4864aebc36f1cf2f6cb01d90d8b6c
bluehead <yiyang.chen@xiyanghui.com>
2018-10-05 18:34:38 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# v2ray-grpc-sdk-php
+
+由protoco生成的[v2ray](https://github.com/v2ray/v2ray-core)的gRPC调用库php版本,使用composer打包方便使用
+
+## 直接安装
+
+## 自行编译
+
+    ./gen.sh
+    
+脚本做了以下事项：
+
+克隆grpc源码，编译插件
+
+克隆v2ray源码利用v2ray自带的protoc二进制文件生成php代码
+
+默认使用mac版本的protoc binary，其它系统请自行修改脚本
+
+
+
+
+
---
+++ gen.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# make protoc-php-plugin which used to gen php code
+#git clone -b $(curl -L https://grpc.io/release) https://github.com/grpc/grpc
+cd grpc
+#git submodule update --init
+#make grpc_php_plugin
+
+cd ../
+git clone https://github.com/v2ray/v2ray-core.git
+cd ./v2ray-core
+# replace package name to dir path
+#sed -ir 's:import "v2ray.com/core/:import ":' ./**/*.proto
+#sed -ir 's#package v2ray.core#package bluehead.v2ray.core#' **/*.proto
+sed -ir 's:import "v2ray.com/core/:import ":' $(find . -type f -name "*.proto")
+sed -ir 's:package v2ray.core:package bluehead.v2ray.core:' $(find . -type f -name "*.proto")
+
+./.dev/protoc/macos/protoc --proto_path=./ --php_out=../src  --grpc_out=../ --plugin=protoc-gen-grpc=./../grpc/bins/opt/grpc_php_plugin ./**/*.proto
+
+echo finished
